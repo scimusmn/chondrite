fb497c33731d4b011eac764fcb6a30b79521caba
bryan kennedy <bkennedy@smm.org>
2014-12-31 17:55:30 -0600

    Adding code to create boilerplate copy

---
+++ server/fixtures.js
@@ -0,0 +1,24 @@
+/**
+ * Fixtures
+ *
+ * If you start up Meteor without any data, fixtures helps define dummy data.
+ */
+
+// Use dimsum to create dummy content in style of the zwordy wifpho nordool
+dimsum.configure({ flavor: 'jabberwocky' });
+
+if (Items.find().count() === 0) {
+
+    var entries = 10;
+    var i = 0;
+    while (i < entries) {
+        i += 1;
+        Items.insert({
+            order: i,
+            link: i,
+            title: dimsum.sentence(1),
+            body: dimsum()
+        });
+    }
+
+}
