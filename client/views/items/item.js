893e76ff4ee399e2633abfeaf4552c774d9a53fb
bryan kennedy <bkennedy@smm.org>
2014-12-31 19:58:39 -0600

    Adding item templates
    Just shows some basic item information.

---
+++ client/views/items/item.js
@@ -0,0 +1,20 @@
+/**
+ * Single item template code
+ */
+Template.item.created = function () {
+    //
+};
+
+Template.item.helpers({
+    myHelper: function () {
+        //
+    }
+});
+
+Template.item.rendered = function () {
+    //
+};
+
+Template.item.events({
+    //
+});
